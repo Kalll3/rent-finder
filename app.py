81c3e0d4158aa5a01d327048a9f3a70f5c29a4cd
Kalll3 <84170439+Kalll3@users.noreply.github.com>
2025-06-19 17:24:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-import api.pandas as pd  # changed here
+import api.pandas as pd  
 
 # Rental items list
 rental_items = [
